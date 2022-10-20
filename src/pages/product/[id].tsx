e78491e59dc9a6d5c87cb6545d0d1b9dd8b9a8cd
Vinicius de Oliveira Pinto <viniciusoliveira9699@gmail.com>
2022-10-20 14:54:18 -0300

    Add Product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,76 @@
-import { useRouter } from 'next/router';
+import Image from "next/future/image"
+import Stripe from "stripe";
+import { stripe } from '../../lib/stripe';
+import { useRouter } from 'next/router'
+import { GetStaticPaths, GetStaticProps } from 'next';
 
-export default function product(){
+import { Button, ImageBoxGradient, ProductContainer, ProductDetails } from '../../styles/pages/product'
+
+import shirtImg from '../../assets/shirt/Variant6.png'
+
+interface ProductProps {
+  product:{
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+
+export default function Product({ product }: ProductProps){
   const {query} = useRouter();
-
+  
   return (
-    <h1>Product {JSON.stringify(query)}</h1>
+    // <h1>Product {JSON.stringify(query)}</h1>
+    <ProductContainer>
+      <ImageBoxGradient>
+        <Image src={product.imageUrl} width={520} height={480} alt=""/>
+      </ImageBoxGradient>
+      
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+        <Button type="button">Comprar agora</Button>
+      </ProductDetails>
+      
+    </ProductContainer>
   );
+}
+
+export const getStaticPaths: GetStaticPaths = async () =>{
+  return {
+    paths: [
+      { params: { id: 'prod_MeGFbxgwtZmAdk' } }
+    ],
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({params}) => {
+  const productId = String(params.id)
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency', 
+          currency: 'BRL' 
+          }).format(price.unit_amount/100),
+        description: product.description,
+      }
+    }, 
+    revalidate: 60 * 60 * 1, // 1 Hour
+  }
 }
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,67 @@
+import { styled } from "..";
+
+export const ProductContainer = styled('main', {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    margin: '0 auto',
+    alignItems: 'stretch',
+    maxWidth: 1180,
+    gap: '4.5rem',
+})
+
+export const ImageBoxGradient = styled('div', {
+    width: '100%',
+    height: 'calc(656px - 0.5rem)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+
+    img: {
+        objectFit: 'cover',
+    }
+})
+
+export const ProductDetails = styled('div', {
+    display: 'flex',
+    flexDirection: 'column',
+    
+    h1: {
+        marginBottom: '1rem',
+        color: '$gray300',
+        fontSize: '$2xl',
+    },
+    span: {
+        color: '$green300',
+        fontSize: '$2xl',
+    },
+    p: {
+        marginTop: '2.5rem',
+        fontSize: '$md',
+        lineHeight: 1.6,
+        color: '$gray300',
+    },
+})
+
+export const Button = styled('button', {
+    width: '100%',
+    height: 70,
+    marginTop: 'auto',
+    padding: '1.25rem',
+
+    background: '$green500',
+    color: '$white',
+    borderRadius: 8,
+    border: 'none',
+    cursor: 'pointer',
+
+    fontSize: '$md',
+    fontWeight: 'bold',
+    transition: 'background, 0.3s',
+
+    '&:hover': {
+        background: '$green300',
+    }
+})
+
